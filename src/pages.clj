35addfa9b71c07ecdbf1ff953f0b934131ae6ab3
Jake Zerrer <him@jakezerrer.com>
2025-08-12 14:46:47 -0400

    Add past work, expand reading list

--- src/pages.clj
+++ src/pages.clj
@@ -4,6 +4,7 @@
 
 (def home-uri "/")
 (def books-2025-uri "/books-2025")
+(def past-work-uri "/past-work")
 (def code-example-uri "/code-example")
 
 (defn template [body]
@@ -29,7 +30,8 @@
        (fn [[uri name]]
          (html
           [:li [:a {:href (str uri)} name]]))
-       [[books-2025-uri "2025 reading list"]])]])))
+       [[books-2025-uri "Selected reading list, 2025"]
+        [past-work-uri "Past work"]])]])))
 
 (defn page [body]
   (template
@@ -43,13 +45,27 @@
   (page
    (html
     [:<>
-     [:h1 "Incomplete 2025 reading list"]
+     [:h1 "Selected reading list, 2025"]
      [:ul
+      [:li "The Second Sex (Simone de Beauvior)"]
       [:li "The Places in Between (Rory Stewart)"]
       [:li "Either/Or: A Fragment of Life (Søren Kierkegaard)"]
       [:li "The Philosophy of History (G. W. F. Hegel)"]
       [:li "This Life: Secular Faith and Spiritual Freedom (Martin Hägglund)"]
+      [:li "The Power Broker (Robert Caro)"]
       [:li "Mating (Normal Rush)"]]])))
+
+(defn past-work
+  []
+  (page
+   (html
+    [:<>
+     [:h1 "Past work"]
+     [:ul
+      [:li "In 2024, I ran product engineering at Normal Computing"]
+      [:li "In the summer of 2023, I traveled and prototyped a devex tool called refuge"]
+      [:li "From 2018 to 2023, I worked as a software engineer at Flexport"]
+      [:li "From 2014 to 2017, I worked as a software engineer at a small startup"]]])))
 
 (defn code-example []
   (page
@@ -65,7 +81,9 @@
 (defn pages []
   {home-uri home
    books-2025-uri books-2025
-   code-example-uri code-example})
+   code-example-uri code-example
+   past-work-uri past-work
+   })
 
 (comment
   (require '[repl :refer [restart build]])
